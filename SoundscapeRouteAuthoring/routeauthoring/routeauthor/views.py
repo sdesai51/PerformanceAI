9864306564c7bdb75e63fcd60af7166ea856f298
Niamh Stockil <nistocki@microsoft.com>
2020-07-29 15:57:22 +0000

    Merged PR 3919: Merging gpx file upload and initial api calls.

--- SoundscapeRouteAuthoring/routeauthoring/routeauthor/views.py
+++ SoundscapeRouteAuthoring/routeauthoring/routeauthor/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from modules.api import *
 
 
 def index(request):
+    if request.method == 'POST' and request.FILES['uploadedGpx']:
+        handle_uploaded_gpx(request.FILES['uploadedGpx'], str(request.FILES['uploadedGpx']))
+        
     return render(request, "personal/webpage.html", {})
 
-
+# debug method
+def handle_uploaded_gpx(file, filename):
+    print(filename)
+    content = file.read()
+    positionData = callConversionApi(content)
+    elevationSegments = callElevationApi(positionData)
