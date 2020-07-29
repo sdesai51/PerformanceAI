b78fb5baa7da7da181dc894b6d16c65134695cf0
Niamh Stockil <nistocki@microsoft.com>
2020-07-29 15:56:30 +0100

    adding gpx upload

--- SoundscapeRouteAuthoring/routeauthoring/routeauthor/views.py
+++ SoundscapeRouteAuthoring/routeauthoring/routeauthor/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+import modules.api
 
 
 def index(request):
+    if request.method == 'POST' and request.FILES['uploadedGpx']:
+        handle_uploaded_gpx(request.FILES['uploadedGpx'], str(request.FILES['uploadedGpx']))
+        
     return render(request, "personal/webpage.html", {})
 
-
+# debug method
+def handle_uploaded_gpx(file, filename):
+    print(filename)
+    content = file.read()
+    print(content)
