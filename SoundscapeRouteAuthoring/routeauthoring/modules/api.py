fea37469ba2e97a401a59808c2ab89c1ebd38cc4
Niamh Stockil <nistocki@microsoft.com>
2020-07-29 18:45:15 +0100

    adding some exception handling

--- SoundscapeRouteAuthoring/routeauthoring/modules/api.py
+++ SoundscapeRouteAuthoring/routeauthoring/modules/api.py
@@ -1,13 +1,21 @@
 import requests
 
 def callConversionApi(gpxContent):
-    r = requests.post("https://microsoftperformanceaiapi.azurewebsites.net/api/Conversion", data=gpxContent)
-    print("Conversion API Call results")
-    print(r.text)
-    return r.text
+    try:
+        r = requests.post("https://microsoftperformanceaiapi.azurewebsites.net/api/Conversion", data=gpxContent)
+        print("Conversion API Call results")
+        print(r.text)
+        return r.text
+    except Exception as e:
+        print('Error calling Conversion API')
+        return ''
 
 def callElevationApi(positionData):
-    r = requests.post("https://microsoftperformanceaiapi.azurewebsites.net/api/Elevation", data=positionData)
-    print("Elevation API Call results")
-    print(r.text)
-    return r.text
+    try:
+        r = requests.post("https://microsoftperformanceaiapi.azurewebsites.net/api/Elevation", data=positionData)
+        print("Elevation API Call results")
+        print(r.text)
+        return r.text
+    except Exception as e:
+        print('Error calling Elevation API')
+        return ''
--- SoundscapeRouteAuthoring/routeauthoring/routeauthor/views.py
+++ SoundscapeRouteAuthoring/routeauthoring/routeauthor/views.py
@@ -5,13 +5,27 @@ from modules.api import *
 
 def index(request):
     if request.method == 'POST' and request.FILES['uploadedGpx']:
-        handle_uploaded_gpx(request.FILES['uploadedGpx'], str(request.FILES['uploadedGpx']))
-        
+        try:
+            elevation = handle_uploaded_gpx(request.FILES['uploadedGpx'], str(request.FILES['uploadedGpx']))
+        except Exception as e:
+            print('The file uploaded has issues')
+
     return render(request, "personal/webpage.html", {})
 
-# debug method
 def handle_uploaded_gpx(file, filename):
     print(filename)
     content = file.read()
+    if content is None:
+         raise Exception('There is no content in this file')
     positionData = callConversionApi(content)
+    if positionData is None:
+         raise Exception('No position data could be found')
     elevationSegments = callElevationApi(positionData)
+    if elevationSegments is None:
+         raise Exception('No elevation segments were generated')
+    return elevationSegments
+
+def convertElevationToWaypoint():
+    print('method to write')
+    # take the elevations
+    # translation to soundscape waypoints
