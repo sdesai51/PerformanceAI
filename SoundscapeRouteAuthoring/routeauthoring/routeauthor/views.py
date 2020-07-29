340b8e4d383abeeeef732f70f5dc107281ce8965
Simran Desai <t-sidesa@microsoft.com>
2020-07-29 17:13:30 -0500

    Populated Preview section using information directly from API.

--- SoundscapeRouteAuthoring/routeauthoring/routeauthor/views.py
+++ SoundscapeRouteAuthoring/routeauthoring/routeauthor/views.py
@@ -3,21 +3,29 @@ from django.http import HttpResponse
 from modules.api import *
 import json
 
-
 def index(request):
     json_data = None
     if request.method == 'POST' and request.FILES['uploadedGpx']:
         try:
+            # elevation is a string JSON objects with elevation added from API calls
             elevation = handle_uploaded_gpx(request.FILES['uploadedGpx'], str(request.FILES['uploadedGpx']))
+            # json_data returns a list of the JSON objects
             json_data = json.loads(elevation)
-            print("Python data loaded from JSON")
-            print(json_data[0]["length"])
-            # print(y["length"])
+           
+            # populate Preview headers   
+            downhills = 0
+            uphills = 0
+            for i, item in enumerate(json_data):
+                if(item["isDownHill"]):
+                    downhills += 1
+                    item["title"] = "Downhill " + str(downhills)
+                else:
+                    uphills += 1
+                    item["title"] = "Uphill " + str(uphills)
         except Exception as e:
             print('The file uploaded has issues')
     
-
-    # convertElevationToWaypoint(elevation)
+    # renders the HTML page and passes json_data to it to render its elements dynamically using this variable
     return render(request, "personal/webpage.html", {'json_data': json_data})
 
 def handle_uploaded_gpx(file, filename):
@@ -31,13 +39,4 @@ def handle_uploaded_gpx(file, filename):
     elevationSegments = callElevationApi(positionData)
     if elevationSegments is None:
          raise Exception('No elevation segments were generated')
-    # print(elevationSegments)
     return elevationSegments
-
-# def convertElevationToWaypoint(elevation):
-#     print('method to write')
-#     # take the elevations
-#     # translation to soundscape waypoints
-
-#     y = json.loads(elevation)
-#     print(y)
