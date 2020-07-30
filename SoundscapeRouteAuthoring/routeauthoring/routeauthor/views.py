c54a03d78ae4bdcc45ad3d28f6b62a5c702f63da
Niamh Stockil <nistocki@microsoft.com>
2020-07-30 13:08:46 +0100

    some code refactoring for readability

--- SoundscapeRouteAuthoring/routeauthoring/routeauthor/views.py
+++ SoundscapeRouteAuthoring/routeauthoring/routeauthor/views.py
@@ -4,29 +4,20 @@ from modules.api import *
 import json
 
 def index(request):
-    json_data = None
+    processed_elevation_data = None
     if request.method == 'POST' and request.FILES['uploadedGpx']:
         try:
-            # elevation is a string JSON objects with elevation added from API calls
-            elevation = handle_uploaded_gpx(request.FILES['uploadedGpx'], str(request.FILES['uploadedGpx']))
-            # json_data returns a list of the JSON objects
-            json_data = json.loads(elevation)
-           
-            # populate Preview headers   
-            downhills = 0
-            uphills = 0
-            for i, item in enumerate(json_data):
-                if(item["isDownHill"]):
-                    downhills += 1
-                    item["title"] = "Downhill " + str(downhills)
-                else:
-                    uphills += 1
-                    item["title"] = "Uphill " + str(uphills)
-        except Exception as e:
+            # elevation_data_str is a string JSON objects with elevation added from API calls
+            elevation_data_str = handle_uploaded_gpx(request.FILES['uploadedGpx'], str(request.FILES['uploadedGpx']))
+            elevation_data = json.loads(elevation_data_str)
+            # prepare data for Preview headers
+            processed_elevation_data = processElevationPoints(elevation_data)
+            
+        except Exception:
             print('The file uploaded has issues')
     
     # renders the HTML page and passes json_data to it to render its elements dynamically using this variable
-    return render(request, "personal/webpage.html", {'json_data': json_data})
+    return render(request, "personal/webpage.html", {'processed_elevation_data': processed_elevation_data})
 
 def handle_uploaded_gpx(file, filename):
     print(filename)
@@ -40,3 +31,16 @@ def handle_uploaded_gpx(file, filename):
     if elevationSegments is None:
          raise Exception('No elevation segments were generated')
     return elevationSegments
+
+def processElevationPoints(elevation_data):
+    print('processing points')
+    downhills = 0
+    uphills = 0
+    for i, item in enumerate(elevation_data):
+        if(item["isDownHill"]):
+            downhills += 1
+            item["title"] = "Downhill " + str(downhills)
+        else:
+            uphills += 1
+            item["title"] = "Uphill " + str(uphills)
+    return elevation_data
